f6cd25811bc620e767e24eb1b01d5847df67d7c7
PaulMickzs <92378318+PaulMickzs@users.noreply.github.com>
2023-02-07 15:43:37 -0300

    Meu primeiro Projeto Js
    Primeira utilização de JavaScript

---
+++ Meu primeiro projeto Js/Meu primeiro projeto Js/script.js
@@ -0,0 +1,416 @@
+// Fantasma
+function fantasma(){
+
+let btn1_fantasma = document.getElementById("btn1");
+let btn2_fantasma = document.getElementById ("btn2");
+let btn3_fantasma = document.getElementById("btn3");
+
+window.alert("Você escondeu o fantasma!");
+ btn2_fantasma.style.display = "none"; 
+ btn1_fantasma.style.display ="none";
+ btn3_fantasma.style.display ="inline-block";
+ 
+
+
+}
+
+// Animais - Cachorro
+
+function cachorro(){
+let img_cachorro = document.getElementById("img_cachorro");
+let btn_cachorro = document.getElementById("cachorro");
+let btn_gato = document.getElementById("gato");
+let btn_coelho = document.getElementById("coelho");
+let btn_hamster = document.getElementById("hamster"); 
+let btn_panda = document.getElementById("panda");
+let btn_pinguim = document.getElementById("pinguim");
+let btn_cavalo = document.getElementById("cavalo");
+let btn_passaro = document.getElementById("passaro");
+let btn_peixe = document.getElementById ("peixe");
+let btn_galinha = document.getElementById ("galinha");
+let btn_unico = document.getElementById("btn4");
+   
+ window.alert("Você escolheu o cachorro!");
+ btn_cachorro.style.display="none";
+ btn_gato.style.display="none";
+ btn_coelho.style.display="none";
+ btn_hamster.style.display="none";
+ btn_panda.style.display="none";
+ btn_pinguim.style.display="none";
+ btn_cavalo.style.display="none";
+ btn_passaro.style.display="none";
+ btn_peixe.style.display="none"; 
+ btn_galinha.style.display="none";
+ img_cachorro.style.display="inline-block";
+ btn_unico.style.display ="inline-block";
+}
+
+// Animais - Gato
+
+function gato(){
+
+    let img_gato = document.getElementById("img_gato");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu o gato!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_gato.style.display="inline-block";
+     btn_unico.style.display ="inline-block";
+}
+
+// Animais - Coelho
+function coelho(){
+
+    let img_coelho = document.getElementById("img_coelho");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu o coelho!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_coelho.style.display="inline-block";
+     btn_unico.style.display ="inline-block";     
+}
+
+// Animais - Hamster
+function hamster(){
+
+    let img_hamster = document.getElementById("img_hamster");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu o hamster!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_hamster.style.display="inline-block";
+     btn_unico.style.display ="inline-block";     
+}
+
+// Animais - Panda
+function panda(){
+
+    let img_panda = document.getElementById("img_panda");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu o panda!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_panda.style.display="inline-block";
+     btn_unico.style.display ="inline-block";     
+}
+
+// Animais - Pinguim
+
+function pinguim(){
+
+    let img_pinguim = document.getElementById("img_pinguim");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu o pinguim!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_pinguim.style.display="inline-block";
+     btn_unico.style.display ="inline-block";     
+}
+
+// Animais - Cavalo
+
+function cavalo(){
+
+    let img_cavalo = document.getElementById("img_cavalo");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu o cavalo!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_cavalo.style.display="inline-block";
+     btn_unico.style.display ="inline-block";     
+}
+
+// Animais - Pássaro
+function passaro(){
+
+    let img_passaro = document.getElementById("img_passaro");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu o pássaro!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_passaro.style.display="inline-block";
+     btn_unico.style.display ="inline-block";     
+}
+
+// Animais - Peixe
+
+function peixe(){
+
+    let img_peixe = document.getElementById("img_peixe");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu o peixe!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_peixe.style.display="inline-block";
+     btn_unico.style.display ="inline-block";     
+}
+
+// Animais - Galinha
+
+function galinha(){
+
+    let img_galinha = document.getElementById("img_galinha");
+    let btn_cachorro = document.getElementById("cachorro");
+    let btn_gato = document.getElementById("gato");
+    let btn_coelho = document.getElementById("coelho");
+    let btn_hamster = document.getElementById("hamster"); 
+    let btn_panda = document.getElementById("panda");
+    let btn_pinguim = document.getElementById("pinguim");
+    let btn_cavalo = document.getElementById("cavalo");
+    let btn_passaro = document.getElementById("passaro");
+    let btn_peixe = document.getElementById ("peixe");
+    let btn_galinha = document.getElementById ("galinha");
+    let btn_unico = document.getElementById("btn4");
+       
+     window.alert("Você escolheu a galinha!");
+     btn_cachorro.style.display="none";
+     btn_gato.style.display="none";
+     btn_coelho.style.display="none";
+     btn_hamster.style.display="none";
+     btn_panda.style.display="none";
+     btn_pinguim.style.display="none";
+     btn_cavalo.style.display="none";
+     btn_passaro.style.display="none";
+     btn_peixe.style.display="none"; 
+     btn_galinha.style.display="none";
+     img_galinha.style.display="inline-block";
+     btn_unico.style.display ="inline-block";     
+}
+
+// Botões de Dúvida
+
+function duvida1(){
+
+   window.alert("Aperte o botão abaixo do fantasma, e veja o que acontece!");
+
+}
+
+function duvida2(){
+      window.alert("Passe o mouse por cima dos botões, e altere a cor deles!");
+}
+
+function duvida3(){
+
+    window.alert("Selecione o seu animal preferido, e tenha uma surpresa!");
+
+}
+
+// Cores
+
+function azul(){
+ 
+     let azul = document.getElementById("primeiro");
+     azul.style.backgroundColor = "blue";
+    
+}
+
+function verde(){
+ 
+    let verde = document.getElementById("segundo");
+    verde.style.backgroundColor = "green";
+}
+
+function rosa(){
+    let rosa = document.getElementById("terceiro");
+    rosa.style.backgroundColor = "magenta";
+}
+
+function amarelo(){
+    let amarelo = document.getElementById("quarto");
+    amarelo.style.backgroundColor = "yellow";
+}
+
+
+function laranja(){
+    let laranja = document.getElementById("quinto");
+    laranja.style.backgroundColor = "orangered";
+}
+
+function marrom(){
+    let marrom = document.getElementById("sexto");
+    marrom.style.backgroundColor = "brown";
+}
+
+
+function roxo(){
+    let roxo = document.getElementById("setimo");
+    roxo.style.backgroundColor = "purple";
+}
+
+
+function violeta(){
+    let violeta = document.getElementById("oitavo");
+    violeta.style.backgroundColor = "violet";
+}
+
+
+function branco(){
+    let branco = document.getElementById("nono");
+    branco.style.backgroundColor = "white";
+}
+
+
+function ciano(){
+    let ciano = document.getElementById("decimo");
+    ciano.style.backgroundColor = "cyan";
+}
+
+function voltar(){
+ 
+    let vermelho = document.getElementById("primeiro");
+    vermelho.style.backgroundColor = "red";
+
+}
+
